fd70a1f05d0d8abb34b03507ec342e47e8b9515c
Joren Synaeve <joren.synaeve@thomasmore.be>
2022-09-30 10:22:03 +0200

    Fix models

--- backend/app/Models/Productlist.php
+++ backend/app/Models/Productlist.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Productlist extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'edition_id',
+        'user_id',
+        'list_number',
+        'is_user_confirmed',
+        'is_employee_validated',
+    ];
 }
