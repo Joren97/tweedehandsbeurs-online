5aa85135848ac946d39c26020b9324ea905f1cdf
Joren Synaeve <joren.synaeve@thomasmore.be>
2022-09-30 11:39:43 +0200

    Add sanctum auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductlistController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/register', [AuthController::class , 'register']);
 Route::post('/auth/login', [AuthController::class , 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('productlist', ProductlistController::class);
+});
