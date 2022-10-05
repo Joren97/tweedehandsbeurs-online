83681576d84535dee483948a2f513a2fa2dec76b
Joren Synaeve <joren.synaeve@thomasmore.be>
2022-10-05 22:19:22 +0200

    Add bootstrap using CDN

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    app: {
+        head: {
+            link: [{
+                rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css'
+            }],
+            script: [
+                { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" }
+            ]
+        }
+    }
 })
