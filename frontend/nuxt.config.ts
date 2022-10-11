20436b8ad77222431096ce5481e31e487af0e5ae
Joren Synaeve <joren.synaeve@thomasmore.be>
2022-10-11 21:48:34 +0200

    Add SB Admin 2 resources

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,8 +6,18 @@ export default defineNuxtConfig({
                 rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css'
             }],
             script: [
-                { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" }
+                { src: "https://kit.fontawesome.com/c0db9975db.js", crossorigin: "anonymous" },
+                { src: "assets/vendor/jquery/jquery.min.js", body: true },
+                { src: "assets/vendor/bootstrap/js/bootstrap.bundle.min.js", body: true },
+                { src: "assets/vendor/jquery-easing/jquery.easing.min.js", body: true },
+                { src: "assets/vendor/chart.js/Chart.min.js" },
+                { src: "assets/js/sb-admin-2.js", body: true },
+                { src: "assets/js/demo/chart-bar-demo.js", body: true },
+                { src: "assets/js/demo/chart-pie-demo.js", body: true },
             ]
         }
-    }
+    },
+    css: [
+        '~/assets/scss/sb-admin-2.scss'
+    ],
 })
