33df1ae336c344d9a4a405bc67b7f7f0121ea8a0
Joren Synaeve <joren.synaeve@thomasmore.be>
2022-10-13 14:45:52 +0200

    Add layout to register page

--- frontend/pages/register.vue
+++ frontend/pages/register.vue
@@ -8,70 +8,72 @@
           <div class="col-lg-7">
             <div class="p-5">
               <div class="text-center">
-                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                <h1 class="h4 text-gray-900 mb-4">Maak een account!</h1>
               </div>
-              <form class="user">
+              <div class="custom-form">
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
                     <input
                       type="text"
-                      class="form-control form-control-user"
+                      class="form-control custom-form-control"
                       id="exampleFirstName"
-                      placeholder="First Name"
+                      placeholder="Voornaam"
+                      v-model="user.firstname"
                     />
                   </div>
                   <div class="col-sm-6">
                     <input
                       type="text"
-                      class="form-control form-control-user"
+                      class="form-control custom-form-control"
                       id="exampleLastName"
-                      placeholder="Last Name"
+                      placeholder="Naam"
+                      v-model="user.lastname"
                     />
                   </div>
                 </div>
                 <div class="form-group">
                   <input
                     type="email"
-                    class="form-control form-control-user"
+                    class="form-control custom-form-control"
                     id="exampleInputEmail"
-                    placeholder="Email Address"
+                    placeholder="Emailadres"
+                    v-model="user.email"
                   />
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
                     <input
                       type="password"
-                      class="form-control form-control-user"
+                      class="form-control custom-form-control"
                       id="exampleInputPassword"
-                      placeholder="Password"
+                      placeholder="Wachtwoord"
+                      v-model="user.password"
                     />
                   </div>
                   <div class="col-sm-6">
                     <input
                       type="password"
-                      class="form-control form-control-user"
+                      class="form-control custom-form-control"
                       id="exampleRepeatPassword"
-                      placeholder="Repeat Password"
+                      placeholder="Bevestig wachtwoord"
+                      v-model="user.passwordConfirm"
                     />
                   </div>
                 </div>
-                <a href="login.html" class="btn btn-primary btn-user btn-block">
-                  Register Account
-                </a>
-                <hr />
-                <a href="index.html" class="btn btn-google btn-user btn-block">
-                  <i class="fab fa-google fa-fw"></i> Register with Google
-                </a>
-                <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                  <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
-                </a>
-              </form>
+                <button class="btn btn-primary custom-btm btn-block" @click="register">
+                  Registreer account
+                </button>
+              </div>
               <hr />
               <div class="text-center">
-                <a class="small" href="forgot-password.html">Forgot Password?</a>
+                <NuxtLink class="small" to="/forgot-password">
+                  Wachtwoord vergeten?
+                </NuxtLink>
               </div>
               <div class="text-center">
-                <a class="small" href="login.html">Already have an account? Login!</a>
+                <NuxtLink class="small" to="/login">
+                  Heb je al een account? Meld je hier aan!
+                </NuxtLink>
               </div>
             </div>
           </div>
@@ -80,3 +82,16 @@
     </div>
   </div>
 </template>
+<script setup>
+const user = ref({
+  firstname: "",
+  lastname: "",
+  email: "",
+  password: "",
+  passwordConfirm: "",
+});
+
+const register = async () => {
+  console.log(user.value);
+};
+</script>
