32d296f41648f72f31c8a4b33ecca474a687a0d7
Joren Synaeve <joren.synaeve@thomasmore.be>
2022-10-24 15:11:42 +0200

    Add overview of lists and modal to add new

--- frontend/pages/lists/index.vue
+++ frontend/pages/lists/index.vue
@@ -1,27 +1,44 @@
 <template>
   <div>
     <LayoutPageHeading>
-      <template v-slot:title>List</template>
+      <template v-slot:title>Mijn lijsten</template>
     </LayoutPageHeading>
-    <p>This is the list page</p>
-    <div class="row">
-      <div class="col-3" v-for="item in editionWithLists">
+    <div class="row row-cols-4">
+      <div class="col mb-3" v-for="item in editionsWithLists">
         <div class="card">
-          <div class="card-header">
-            {{ item.id }} &dash;{{ item.year }} &dash; {{ item.name }}
-          </div>
+          <div class="card-header">{{ item.year }} &dash; {{ item.name }}</div>
           <ul class="list-group list-group-flush">
-            <li class="list-group-item" v-for="item in item.lists">
-              {{ item.listNumber }}
-            </li>
+            <template v-if="item.lists.length > 0">
+              <li class="list-group-item pe-clickable" v-for="item in item.lists">
+                Lijst {{ item.listNumber }}
+                <span v-if="item.memberNumber" class="text-muted small"
+                  >({{ item.memberNumber }})</span
+                >
+              </li>
+            </template>
+            <template v-else><li class="list-group-item">Geen lijsten</li> </template>
           </ul>
         </div>
       </div>
     </div>
+    <button
+      type="button"
+      class="btn btn-primary"
+      data-bs-toggle="modal"
+      data-bs-target="#newListModal"
+    >
+      Nieuwe lijst toevoegen
+    </button>
+    <div
+      class="modal fade"
+      id="newListModal"
+      tabindex="-1"
+      aria-labelledby="newListModalLabel"
+      aria-hidden="true"
+    >
+      <NewListModal />
+    </div>
   </div>
-  <p>{{ editionWithLists }}</p>
-  <br />
-  <p>{{ lists }}</p>
 </template>
 <script setup>
 definePageMeta({
@@ -40,4 +57,18 @@ const {
   pending: listsPending,
   data: listsData,
 } = useCustomLazyFetch(`/api/productlist/me`, { key: "productlist" });
+
+const editionsWithLists = computed(() => {
+  if (!editionsData) return [];
+  if (!editionsData.value) return [];
+  if (!listsData) return [];
+  if (!listsData.value) return [];
+
+  return editionsData.value.data.map((edition) => {
+    return {
+      ...edition,
+      lists: listsData.value.data.filter((list) => list.editionId === edition.id),
+    };
+  });
+});
 </script>
