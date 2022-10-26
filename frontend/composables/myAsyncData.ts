fac0d6662e9b9b708e654654c3216e93241b4f41
Joren Synaeve <joren.synaeve@thomasmore.be>
2022-10-26 11:50:01 +0200

    Add myAsyncData composable

---
+++ frontend/composables/myAsyncData.ts
@@ -0,0 +1,28 @@
+import { AsyncDataOptions, UseFetchOptions } from "nuxt/dist/app/composables";
+
+export const myAsyncData = (url: Function, fetchOptions?: UseFetchOptions<unknown>, asyncDataOptions?: AsyncDataOptions<unknown>) => {
+    const config = useRuntimeConfig();
+
+    return useAsyncData(() =>
+        $fetch(
+            config.public.API_BASE_URL + url(),
+            {
+                ...fetchOptions,
+                async onRequest({ request, options }) {
+                    const token = useCookie("apiToken");
+
+                    if (token.value) {
+                        options.headers = {
+                            ...options.headers,
+                            Authorization: `Bearer ${token.value}`,
+                        };
+                    }
+
+                    console.log("[fetch request]");
+                }
+            }
+        ),
+        {
+            ...asyncDataOptions
+        });
+};
