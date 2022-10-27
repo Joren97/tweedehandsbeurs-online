f6b4849db13f8da0db920169bfcde4ef183ff8b2
Matthias Claessen <r0459796@student.thomasmore.be>
2022-10-27 23:37:27 +0200

    Added scss for components and Google font

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,45 +1,48 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    ssr: false,
-    target: 'static',
-    app: {
-        head: {
-            link: [{
-                rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css'
-            }],
-            script: [
-                { src: "https://kit.fontawesome.com/c0db9975db.js", crossorigin: "anonymous" },
-                { src: "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" },
-                {
-                    src: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js",
-                    crossorigin: "anonymous", integrity: "sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-                },
-                { src: "./assets/js/sb-admin-2.js", body: true },
-            ]
+  ssr: false,
+  target: 'static',
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700&display=swap',
         },
+      ],
+      script: [
+        { src: 'https://kit.fontawesome.com/c0db9975db.js', crossorigin: 'anonymous' },
+        { src: 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js' },
+        {
+          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+          crossorigin: 'anonymous',
+          integrity: 'sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM',
+        },
+        { src: 'assets/js/sb-admin-2.js', body: true },
+        { src: 'assets/js/main.js', body: true },
+      ],
     },
-    css: [
-        '~/assets/scss/sb-admin-2.scss'
-    ],
-    modules: [
-        [
-            '@pinia/nuxt',
-            {
-                autoImports: [
-                    // automatically imports `defineStore`
-                    'defineStore', // import { defineStore } from 'pinia'
-                    // automatically imports `defineStore` as `definePiniaStore`
-                    ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
-                ],
-            },
+  },
+  css: ['~/assets/scss/main.scss'],
+  modules: [
+    [
+      '@pinia/nuxt',
+      {
+        autoImports: [
+          // automatically imports `defineStore`
+          'defineStore', // import { defineStore } from 'pinia'
+          // automatically imports `defineStore` as `definePiniaStore`
+          ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
         ],
+      },
     ],
-    typescript: {
-        typeCheck: true
+  ],
+  typescript: {
+    typeCheck: true,
+  },
+  runtimeConfig: {
+    public: {
+      API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8000',
     },
-    runtimeConfig: {
-        public: {
-            API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8000'
-        }
-    }
-})
+  },
+});
--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,87 +1,52 @@
 <template>
-  <div class="container">
-    <!-- Outer Row -->
-    <div class="row justify-content-center">
-      <div class="col-xl-10 col-lg-12 col-md-9">
-        <div class="card o-hidden border-0 shadow-lg my-5">
-          <div class="card-body p-0">
-            <!-- Nested Row within Card Body -->
-            <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-              <div class="col-lg-6">
-                <div class="p-5">
-                  <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Welkom terug!</h1>
-                  </div>
-                  <div class="custom-form">
-                    <div class="form-group">
-                      <input
-                        type="email"
-                        class="form-control custom-form-control"
-                        id="exampleInputEmail"
-                        aria-describedby="emailHelp"
-                        placeholder="Jouw email"
-                        v-model="user.email"
-                      />
-                      <p v-if="fieldErrors.email">{{ fieldErrors.email }}</p>
-                    </div>
-                    <div class="form-group">
-                      <input
-                        type="password"
-                        class="form-control custom-form-control"
-                        id="exampleInputPassword"
-                        placeholder="Jouw wachtwoord"
-                        v-model="user.password"
-                      />
-                      <p v-if="fieldErrors.password">{{ fieldErrors.password }}</p>
-                    </div>
-                    <div class="form-group">
-                      <div class="custom-control custom-checkbox small">
-                        <input
-                          type="checkbox"
-                          class="custom-control-input"
-                          id="customCheck"
-                          v-model="user.remember"
-                        />
-                        <label class="custom-control-label" for="customCheck"
-                          >Remember Me</label
-                        >
-                      </div>
-                    </div>
-                    <button class="btn btn-primary custom-btm btn-block" @click="login">
-                      Login
-                    </button>
-                  </div>
-                  <hr />
-                  <div class="text-center">
-                    <a class="small" href="forgot-password.html">Forgot Password?</a>
-                  </div>
-                  <div class="text-center">
-                    <a class="small" href="register.html">Create an Account!</a>
-                  </div>
-                </div>
-              </div>
-            </div>
+  <section class="section__login">
+    <div class="login__form">
+      <div class="form__image">
+        <img src="~/assets/img/clothes.jpg" />
+      </div>
+      <div class="form__form">
+        <div class="form__title">
+          <h1 class="">Welkom terug!</h1>
+        </div>
+        <div class="form-group">
+          <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Jouw email" v-model="user.email" />
+          <p v-if="fieldErrors.email">{{ fieldErrors.email }}</p>
+        </div>
+        <div class="form-group">
+          <input type="password" class="form-control" id="exampleInputPassword" placeholder="Jouw wachtwoord" v-model="user.password" />
+          <p v-if="fieldErrors.password">{{ fieldErrors.password }}</p>
+        </div>
+        <div class="form-group">
+          <input type="checkbox" class="form-checkbox" id="customCheck" v-model="user.remember" />
+          <label class="form-checkbox-label" for="customCheck">Onthoud mij</label>
+        </div>
+        <button class="btn btn-login" @click="login">Login</button>
+        <div class="form__options">
+          <div class="options__forgot">
+            <NuxtLink class="small" to="/forgot-password"> Wachtwoord vergeten? </NuxtLink>
+          </div>
+          <div class="options__register">
+            <NuxtLink class="small" to="/register"> Maak een account!</NuxtLink>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 <script setup>
 const user = ref({
-  email: "",
-  password: "",
+  email: '',
+  password: '',
   remember: false,
 });
 
 const fieldErrors = ref({});
 
 const login = async () => {
-  console.log("login");
+  console.log('login');
 
-  const { data, error } = await useCustomFetch("/api/auth/login", {
-    method: "POST",
+  const { data, error } = await useCustomFetch('/api/auth/login', {
+    method: 'POST',
     body: user.value,
     initialCache: false,
   });
@@ -93,8 +58,8 @@ const login = async () => {
 
   let maxAge = null;
   if (data.value.data.remember) maxAge = 604800;
-  const token = useCookie("apiToken", { maxAge });
+  const token = useCookie('apiToken', { maxAge });
   token.value = data.value.data.token;
-  navigateTo("/");
+  navigateTo('/');
 };
 </script>
