5de34bd4d06776f0abaf9d0c6488ae2681777265
Joren Synaeve <joren.synaeve@thomasmore.be>
2022-11-02 12:20:12 +0100

    Make form submitable by pressing enter

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -4,49 +4,69 @@
       <div class="form__image">
         <img src="~/assets/img/clothes.jpg" />
       </div>
-      <div class="form__form">
+      <form class="form__form" @submit.prevent="login">
         <div class="form__title">
           <h1 class="">Welkom terug!</h1>
         </div>
         <div class="form-group">
-          <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Jouw email" v-model="user.email" />
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail"
+            aria-describedby="emailHelp"
+            placeholder="Jouw email"
+            v-model="user.email"
+          />
           <p v-if="fieldErrors.email">{{ fieldErrors.email }}</p>
         </div>
         <div class="form-group">
-          <input type="password" class="form-control" id="exampleInputPassword" placeholder="Jouw wachtwoord" v-model="user.password" />
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword"
+            placeholder="Jouw wachtwoord"
+            v-model="user.password"
+          />
           <p v-if="fieldErrors.password">{{ fieldErrors.password }}</p>
         </div>
         <div class="form-group">
-          <input type="checkbox" class="form-checkbox" id="customCheck" v-model="user.remember" />
+          <input
+            type="checkbox"
+            class="form-checkbox"
+            id="customCheck"
+            v-model="user.remember"
+          />
           <label class="form-checkbox-label" for="customCheck">Onthoud mij</label>
         </div>
-        <button class="btn btn-login" @click="login">Login</button>
+        <button class="btn btn-login" type="submit">Login</button>
         <div class="form__options">
           <div class="options__forgot">
-            <NuxtLink class="small" to="/forgot-password"> Wachtwoord vergeten? </NuxtLink>
+            <NuxtLink class="small" to="/forgot-password">
+              Wachtwoord vergeten?
+            </NuxtLink>
           </div>
           <div class="options__register">
             <NuxtLink class="small" to="/register"> Maak een account!</NuxtLink>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   </section>
 </template>
 <script setup>
 const user = ref({
-  email: '',
-  password: '',
+  email: "",
+  password: "",
   remember: false,
 });
 
 const fieldErrors = ref({});
 
 const login = async () => {
-  console.log('login');
+  console.log("login");
 
-  const { data, error } = await useCustomFetch('/api/auth/login', {
-    method: 'POST',
+  const { data, error } = await useCustomFetch("/api/auth/login", {
+    method: "POST",
     body: user.value,
     initialCache: false,
   });
@@ -58,8 +78,8 @@ const login = async () => {
 
   let maxAge = null;
   if (data.value.data.remember) maxAge = 604800;
-  const token = useCookie('apiToken', { maxAge });
+  const token = useCookie("apiToken", { maxAge });
   token.value = data.value.data.token;
-  navigateTo('/');
+  navigateTo("/");
 };
 </script>
