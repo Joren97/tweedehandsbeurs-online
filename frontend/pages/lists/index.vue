70bfeb8bcefaef80525d718dac1f974f40ed181d
Joren Synaeve <joren.synaeve@thomasmore.be>
2022-12-07 10:58:01 +0100

    Add pending

--- frontend/pages/lists/index.vue
+++ frontend/pages/lists/index.vue
@@ -1,23 +1,39 @@
 <template>
   <section class="dashboard__lists">
-    <div class="lists__title">
-      Mijn lijsten
-    </div>
+    <div class="lists__title">Mijn lijsten</div>
 
     <div class="lists__grid">
-      <div class="grid__item" v-for="item in editionsWithLists" :key="item.id">
+      <div class="grid__item placeholder-glow" v-if="pending" v-for="i in 4">
+        <div class="item__title"><span class="placeholder col-4"></span></div>
+        <div class="item__lists">
+          <div class="lists__emtpy"><span class="placeholder col-12"></span></div>
+        </div>
+      </div>
+      <div class="grid__item" v-for="item in editionsWithLists" :key="item.id" v-else>
         <div class="item__title">{{ item.year }} - {{ item.name }}</div>
         <div class="item__lists" v-if="item.lists.length > 0">
-          <div class="lists__item" v-for="list in item.lists" :key="list.id" @click="openList(list.id)">{{ list.id }} Lijst {{ list.listNumber }} ({{ list.memberNumber }})</div>
+          <div
+            class="lists__item"
+            v-for="list in item.lists"
+            :key="list.id"
+            @click="openList(list.id)"
+          >
+            {{ list.id }} Lijst {{ list.listNumber }} ({{ list.memberNumber }})
+          </div>
         </div>
         <div class="item__lists" v-else>
-          <div class="lists__emtpy">
-            Geen lijsten
-          </div>
+          <div class="lists__emtpy">Geen lijsten</div>
         </div>
       </div>
     </div>
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newListModal">Nieuwe lijst toevoegen</button>
+    <button
+      type="button"
+      class="btn btn-primary"
+      data-bs-toggle="modal"
+      data-bs-target="#newListModal"
+    >
+      Nieuwe lijst toevoegen
+    </button>
     <div class="modal fade d-none" id="newListModal">
       <NewListModal :active-edition="activeEdition" @list-created="onListCreated" />
     </div>
@@ -26,18 +42,29 @@
 
 <script setup>
 useHead({
-  title: 'Mijn lijsten',
+  title: "Mijn lijsten",
 });
 definePageMeta({
-  layout: 'dashboard',
-  middleware: ['auth'],
+  layout: "dashboard",
+  middleware: ["auth"],
   meta: {
-    authLevel: 'user',
+    authLevel: "user",
   },
 });
 
-const { pending: editionsPending, data: editionsData } = myLazyFetch(() => `/api/edition`, { key: 'edition', initialCache: false });
-const { pending: listsPending, data: listsData, refresh: listsRefresh } = myLazyFetch(() => `/api/productlist/me`, { key: 'productlist', initialCache: false });
+const { pending: editionsPending, data: editionsData } = myLazyFetch(
+  () => `/api/edition`,
+  { key: "edition", initialCache: false }
+);
+const {
+  pending: listsPending,
+  data: listsData,
+  refresh: listsRefresh,
+} = myLazyFetch(() => `/api/productlist/me`, { key: "productlist", initialCache: false });
+
+const pending = computed(() => {
+  return editionsPending.value || listsPending.value;
+});
 
 const editionsWithLists = computed(() => {
   if (!editionsData) return [];
