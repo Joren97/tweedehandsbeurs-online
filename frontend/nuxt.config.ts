ef98847c72be544bae7a5d98e1c6fcc546a2d52f
JorenSynaeveTM <joren.synaeve@thomasmore.be>
2022-12-08 09:53:40 +0100

    Remove unused scripts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -17,9 +17,7 @@ export default defineNuxtConfig({
           src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
           crossorigin: 'anonymous',
           integrity: 'sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM',
-        },
-        { src: 'assets/js/sb-admin-2.js', body: true },
-        { src: 'assets/js/main.js', body: true },
+        }
       ],
     },
   },
