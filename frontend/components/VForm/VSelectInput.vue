f96c1915f712b07d89b686dd88b7de7b82d4e2fc
JorenSynaeveTM <joren.synaeve@thomasmore.be>
2023-01-09 09:05:03 +0100

    Add custom select component

---
+++ frontend/components/VForm/VSelectInput.vue
@@ -0,0 +1,46 @@
+<template>
+  <VField :name="name" v-slot="{ field, meta, errors }">
+    <select
+      :id="name"
+      :disabled="disabled"
+      v-bind="field"
+      class="form-select"
+      :class="{
+        'is-valid': meta.valid && meta.touched,
+        'is-invalid': !meta.valid && meta.touched,
+      }"
+      :placeholder="placeholder"
+    >
+      <slot></slot>
+    </select>
+
+    <VErrorMessage :name="name" as="div" class="invalid-feedback" />
+  </VField>
+</template>
+
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    default: "text",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  placeholder: {
+    type: String,
+    default: "",
+  },
+  leftIcon: {
+    type: String,
+    default: "",
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style></style>
