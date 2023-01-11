a73b64776eeb2923b02546f2c7255a9c869204b3
JorenSynaeveTM <joren.synaeve@thomasmore.be>
2023-01-11 20:59:56 +0100

    Rework emails

--- backend/resources/views/pdfs/list.blade.php
+++ backend/resources/views/pdfs/list.blade.php
@@ -2,13 +2,31 @@
 <html>
 
 <head>
-    <title>Productlist</title>
+    <title>Lijst {{$list->list_number}}</title>
 </head>
 
 <body>
-    <p>Hier komt een lijst van de producten</p>
-
-    <p>Thank you</p>
+    <h1>Lijst {{$list->list_number}}</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Beschrijving</th>
+                <th>Vraagprijs</th>
+                <th>Verkoopprijs</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($list->products as $product)
+            <tr>
+                <td>{{$product->product_number}}</td>
+                <td>{{$product->description}}</td>
+                <td>{{$product->price->asking_price}}</td>
+                <td>{{$product->price->selling_price}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </body>
 
 </html>
