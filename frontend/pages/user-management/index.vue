bdae0addc30919da1cf458d8b95cfc2a8cbe5745
JorenSynaeveTM <joren.synaeve@thomasmore.be>
2023-01-12 16:14:43 +0100

    Add loading and pagination

--- frontend/pages/user-management/index.vue
+++ frontend/pages/user-management/index.vue
@@ -3,44 +3,53 @@
     <LayoutPageHeading>
       <template v-slot:title>Gebruikers</template>
     </LayoutPageHeading>
-    <table class="users__table datatable">
-      <thead>
-        <tr>
-          <th>Voornaam</th>
-          <th>Achternaam</th>
-          <th>Email</th>
-          <th>Lidnummer</th>
-          <th>Telefoon</th>
-          <th>Adres</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody v-if="pending" class="placeholder-glow">
-        <tr v-for="i in 15">
-          <td><span class="placeholder w-100"></span></td>
-          <td><span class="placeholder w-100"></span></td>
-          <td><span class="placeholder w-100"></span></td>
-          <td><span class="placeholder w-100"></span></td>
-          <td><span class="placeholder w-100"></span></td>
-          <td><span class="placeholder w-100"></span></td>
-          <td></td>
-        </tr>
-      </tbody>
-      <tbody v-else>
-        <tr v-for="user in users" :key="user.id">
-          <td>{{ emptyCheck(user.firstname) }}</td>
-          <td>{{ emptyCheck(user.lastname) }}</td>
-          <td>{{ emptyCheck(user.email) }}</td>
-          <td>{{ emptyCheck(user.memberNumber) }}</td>
-          <td>{{ user.phoneNumber }}</td>
-          <td>{{ formatAdress(user) }}</td>
-          <td class="datatable__actions">
-            <span class="divider"></span>
-            <span class="action"><i class="fa-regular fa-eye fa-lg"></i></span>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="row mb-3">
+      <div class="col">
+        <table class="users__table datatable">
+          <thead>
+            <tr>
+              <th>Voornaam</th>
+              <th>Achternaam</th>
+              <th>Email</th>
+              <th>Lidnummer</th>
+              <th>Telefoon</th>
+              <th>Adres</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody v-if="pending" class="placeholder-glow">
+            <tr v-for="i in 15">
+              <td><span class="placeholder w-100"></span></td>
+              <td><span class="placeholder w-100"></span></td>
+              <td><span class="placeholder w-100"></span></td>
+              <td><span class="placeholder w-100"></span></td>
+              <td><span class="placeholder w-100"></span></td>
+              <td><span class="placeholder w-100"></span></td>
+              <td></td>
+            </tr>
+          </tbody>
+          <tbody v-else>
+            <tr v-for="user in users" :key="user.id">
+              <td>{{ emptyCheck(user.firstname) }}</td>
+              <td>{{ emptyCheck(user.lastname) }}</td>
+              <td>{{ emptyCheck(user.email) }}</td>
+              <td>{{ emptyCheck(user.memberNumber) }}</td>
+              <td>{{ user.phoneNumber }}</td>
+              <td>{{ formatAdress(user) }}</td>
+              <td class="datatable__actions">
+                <span class="divider"></span>
+                <span class="action"><i class="fa-regular fa-eye fa-lg"></i></span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <Pagination :pagination="pagination" />
+      </div>
+    </div>
   </section>
 </template>
 <script setup>
@@ -90,4 +99,10 @@ const formatAdress = (user) => {
   // Return address, postalCode and city
   return `${user.address}, ${user.postalCode} ${user.city}`;
 };
+
+const pagination = computed(() => {
+  if (!data) return {};
+  if (!data.value) return {};
+  return data.value.meta;
+});
 </script>
