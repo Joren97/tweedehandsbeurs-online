f050fd1ce023b4017746bdb0f8ecbc189afeb748
JorenSynaeveTM <joren.synaeve@thomasmore.be>
2023-01-12 08:20:16 +0100

    Add styling

--- backend/resources/views/pdfs/list.blade.php
+++ backend/resources/views/pdfs/list.blade.php
@@ -5,26 +5,72 @@
     <title>Lijst {{$list->list_number}}</title>
 </head>
 
+<style>
+    html {
+        font-family: 'Roboto', sans-serif;
+    }
+
+    .table__products {
+    width: 100%;
+    border-collapse: separate !important;
+    overflow: hidden;
+    border-radius: 0.25rem 0.25rem 0 0;
+    border-spacing: 0;
+    border: 1px solid #e9ecef;
+
+    }
+
+    .table__products th {
+      border: 1px solid #e9ecef;
+      padding: 0.5rem;
+      vertical-align: middle;
+    }
+
+    .table__products td {
+      border: 1px solid #e9ecef;
+      padding: 0.25rem;
+      vertical-align: middle;
+    }
+
+    .table__products tbody tr:nth-child(2n) {
+        background-color: #f5f5f5;
+      }
+
+    .table__products .product__data {
+      padding-left: 1rem;
+    }
+
+    .table__products .product__number {
+      text-align: center;
+    }
+
+    .table__products .product__buttons {
+      display: flex;
+      justify-content: space-evenly;      
+    }
+</style>
+
 <body>
     <h1>Lijst {{$list->list_number}}</h1>
-    <table>
+
+    <table class="table__products">
         <thead>
-            <tr>
-                <th>#</th>
-                <th>Beschrijving</th>
-                <th>Vraagprijs</th>
-                <th>Verkoopprijs</th>
-            </tr>
+        <tr>
+            <th class="product__number">#</th>
+            <th class="product__data">Beschrijving</th>
+            <th class="product__data">Vraagprijs</th>
+            <th class="product__data">Verkoopprijs</th>
+        </tr>
         </thead>
         <tbody>
-            @foreach ($list->products as $product)
-            <tr>
-                <td>{{$product->product_number}}</td>
-                <td>{{$product->description}}</td>
-                <td>{{$product->price->asking_price}}</td>
-                <td>{{$product->price->selling_price}}</td>
-            </tr>
-            @endforeach
+        @foreach ($list->products as $product)
+        <tr>
+            <td class="product__number">{{$product->product_number}}</td>
+            <td class="product__data">{{$product->description}}</td>
+            <td class="product__data">&euro;&nbsp;{{str_replace('.',',',$product->price->asking_price)}}</td>
+            <td class="product__data">&euro;&nbsp;{{str_replace('.',',',$product->price->selling_price)}}</td>
+        </tr>
+        @endforeach
         </tbody>
     </table>
 </body>
