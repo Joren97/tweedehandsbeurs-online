6016decd858dcfe413bc9efa0911e63458fe28b0
JorenSynaeveTM <joren.synaeve@thomasmore.be>
2023-01-12 15:47:50 +0100

    Create user controller

---
+++ backend/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Filters\UserFilter;
+use App\Http\Resources\UserCollection;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \App\Http\Resources\UserCollection
+     */
+    public function index(Request $request)
+    {
+        $filter = new UserFilter();
+        $filterItems = $filter->transform($request);
+
+        $users = User::where($filterItems);
+
+        return new UserCollection($users->orderBy('lastname')->paginate()->appends($request->query()));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
