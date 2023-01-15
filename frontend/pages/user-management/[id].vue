ba4201f1004336a7d8813dcf32e516ff990b1368
JorenSynaeveTM <joren.synaeve@thomasmore.be>
2023-01-15 16:03:07 +0100

    Add page with css and link to page

---
+++ frontend/pages/user-management/[id].vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="section__user-management-detail">
+    <div class="title">Joren Synaeve</div>
+    Gebruikersinfo
+  </section>
+</template>
+<script>
+definePageMeta({
+  layout: "dashboard",
+  middleware: ["auth"],
+  meta: {
+    authLevel: "employee",
+  },
+});
+</script>
