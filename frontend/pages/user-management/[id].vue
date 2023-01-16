de329c0bd5d8f0855e884fb5049e647966eb6bdb
JorenSynaeveTM <joren.synaeve@thomasmore.be>
2023-01-16 14:14:06 +0100

    Rework page layout

--- frontend/pages/user-management/[id].vue
+++ frontend/pages/user-management/[id].vue
@@ -2,7 +2,8 @@
   <section class="section__user-management-detail">
     <div class="title">Gebruikersinfo</div>
     <div class="row">
-      <div class="col">
+      <div class="subtitle">Huidige editie</div>
+      <div class="col-3">
         <div class="list__information">
           <div class="information__title">
             <span>{{ user.firstname }} {{ user.lastname }}</span>
@@ -26,23 +27,27 @@
             <div class="information__item mb-2">
               <span class="item__title">Adres</span>
               <span v-if="userPending" class="placeholder col-4"></span>
-              <span v-else>TODO</span>
+              <span v-else>{{ formatAdress(user) }}</span>
             </div>
           </div>
         </div>
       </div>
       <div class="col">
-        <p class="subtitle">Huidige editie</p>
-        <div
-          class="list__information mb-3"
-          v-for="item in user.productlists"
-          :key="item.id"
-        >
+        <div class="list__information mb-3">
           <div class="information__title">
             <span v-if="userPending" class="placeholder col-4"></span>
-            <span v-else>Lijstnummer {{ item.listNumber }}</span>
+            <span v-else>Lijstoverzicht</span>
           </div>
-          <div class="information__content placeholder-glow">
+          <div
+            class="information__content placeholder-glow"
+            v-for="item in user.productlists"
+            :key="item.id"
+          >
+            <div class="information__item mb-2">
+              <span class="item__title">Lijstnummer</span>
+              <span v-if="userPending" class="placeholder col-4"></span>
+              <span v-else>{{ emptyCheck(item.listNumber) }}</span>
+            </div>
             <div class="information__item mb-2">
               <span class="item__title">Lidnummer</span>
               <span v-if="userPending" class="placeholder col-4"></span>
@@ -64,53 +69,32 @@
               <span v-else><YesNoIcon :value="item.isPaidToUser" /></span>
             </div>
             <div class="information__item">
-              <span class="item__title">Jouw opbrengst</span>
+              <span class="item__title">Opbrengst</span>
               <span v-if="userPending" class="placeholder col-4"></span>
               <span v-else>{{ toEuro(item.userProfit) }}</span>
             </div>
+            <hr />
           </div>
         </div>
         <p>{{ user.productlists }}</p>
       </div>
+    </div>
+    <div class="row">
       <div class="col">
         <p class="subtitle">Geschiedenis</p>
         <div class="list__accordion">
-          <div class="collapsible-accordion" v-for="item in history">
+          <div class="collapsible-accordion" v-for="item in user.history">
             <div class="collapsible-item">
-              <input type="checkbox" id="tab1" />
-              <label class="collapsible-item-label" for="tab1"
+              <input type="checkbox" :id="`item-${item.id}`" />
+              <label class="collapsible-item-label" :for="`item-${item.id}`"
                 >{{ item.name }}&nbsp;{{ item.year }}</label
               >
               <div class="collapsible-item-content">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum,
-                reiciendis!
-              </div>
-            </div>
-          </div>
-          <div class="collapsible-accordion">
-            <div class="collapsible-item">
-              <input type="checkbox" id="tab2" />
-              <label class="collapsible-item-label" for="tab2">Item 2</label>
-              <div class="collapsible-item-content">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum,
-                reiciendis!
-              </div>
-            </div>
-          </div>
-          <div class="collapsible-accordion">
-            <div class="collapsible-item">
-              <input type="checkbox" id="tab3" />
-              <label class="collapsible-item-label" for="tab3">Item 3</label>
-              <div class="collapsible-item-content">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum,
-                reiciendis!
+                <p v-for="list in item.productlists">Lijst {{ list.listNumber }}</p>
               </div>
             </div>
           </div>
         </div>
-
-        <!-- <p>{{ user.listHistory }}</p> -->
-        <p>{{ history }}</p>
       </div>
     </div>
   </section>
@@ -139,30 +123,5 @@ const user = computed(() => {
   if (!userData) return {};
   if (!userData.value) return {};
   return userData.value.data;
-});
-
-const history = computed(() => {
-  if (!userData) return {};
-  if (!userData.value) return {};
-
-  let editions = [];
-
-  // Get all editions from listHistory
-  userData.value.data.listHistory.forEach((list) => {
-    if (!editions.includes(list.edition)) {
-      editions.push({ ...list.edition, lists: [] });
-    }
-  });
-
-  // Add all lists to their respective edition
-  // editions.forEach((edition) => {
-  //   userData.value.data.listHistory.forEach((list) => {
-  //     if (list.editionId === edition.id) {
-  //       edition.lists.push(list);
-  //     }
-  //   });
-  // });
-
-  return editions;
 });
 </script>
