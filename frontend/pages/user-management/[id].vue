623e9a3c54ba268571628aa5579a2d3c2b3eba2c
JorenSynaeveTM <joren.synaeve@thomasmore.be>
2023-01-17 15:14:08 +0100

    Get layout right

--- frontend/pages/user-management/[id].vue
+++ frontend/pages/user-management/[id].vue
@@ -33,57 +33,33 @@
         </div>
       </div>
       <div class="col">
-        <div class="list__information mb-3">
-          <div class="information__title">
-            <span v-if="userPending" class="placeholder col-4"></span>
-            <span v-else>Lijstoverzicht</span>
-          </div>
-          <div
-            class="information__content placeholder-glow"
-            v-for="item in user.productlists"
-            :key="item.id"
-          >
-            <div class="information__item mb-2">
-              <span class="item__title">Lijstnummer</span>
-              <span v-if="userPending" class="placeholder col-4"></span>
-              <span v-else>{{ emptyCheck(item.listNumber) }}</span>
-            </div>
-            <div class="information__item mb-2">
-              <span class="item__title">Lidnummer</span>
-              <span v-if="userPending" class="placeholder col-4"></span>
-              <span v-else>{{ emptyCheck(item.memberNumber) }}</span>
-            </div>
-            <div class="information__item mb-2">
-              <span class="item__title">Bevestigd</span>
-              <span v-if="userPending" class="placeholder col-4"></span>
-              <span v-else><YesNoIcon :value="item.isUserConfirmed" /></span>
-            </div>
-            <div class="information__item mb-2">
-              <span class="item__title">Gevalideerd</span>
-              <span v-if="userPending" class="placeholder col-4"></span>
-              <span v-else><YesNoIcon :value="item.isEmployeeValidated" /></span>
-            </div>
-            <div class="information__item mb-2">
-              <span class="item__title">Opbrengst uitbetaald</span>
-              <span v-if="userPending" class="placeholder col-4"></span>
-              <span v-else><YesNoIcon :value="item.isPaidToUser" /></span>
-            </div>
-            <div class="information__item">
-              <span class="item__title">Opbrengst</span>
-              <span v-if="userPending" class="placeholder col-4"></span>
-              <span v-else>{{ toEuro(item.userProfit) }}</span>
-            </div>
-            <hr />
-          </div>
-        </div>
-        <p>{{ user.productlists }}</p>
+        <table class="datatable">
+          <thead>
+            <th>Lijstnummer</th>
+            <th>Lidnummer</th>
+            <th>Bevestigd</th>
+            <th>Gevalideerd</th>
+            <th>Uitbetaald</th>
+            <th>Opbrengst</th>
+          </thead>
+          <tbody>
+            <tr v-for="item in current" :key="item.id">
+              <td>{{ emptyCheck(item.listNumber) }}</td>
+              <td>{{ emptyCheck(item.memberNumber) }}</td>
+              <td><YesNoIcon :value="item.isUserConfirmed" /></td>
+              <td><YesNoIcon :value="item.isEmployeeValidated" /></td>
+              <td><YesNoIcon :value="item.isPaidToUser" /></td>
+              <td>{{ toEuro(item.userProfit) }}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
-    <div class="row">
+    <div class="row mt-3">
       <div class="col">
         <p class="subtitle">Geschiedenis</p>
         <div class="list__accordion">
-          <div class="collapsible-accordion" v-for="item in user.history">
+          <div class="collapsible-accordion" v-for="item in editionHistory">
             <div class="collapsible-item">
               <input type="checkbox" :id="`item-${item.id}`" />
               <label class="collapsible-item-label" :for="`item-${item.id}`"
@@ -112,9 +88,27 @@ const { data: userData, pending: userPending } = myLazyFetch(
   () => `/api/user/${useRoute().params.id}`,
   {
     key: "user",
+  }
+);
+
+const { data: historyData, pending: historyPending } = myLazyFetch(
+  () => `/api/productlist`,
+  {
+    key: "history",
     params: {
-      includeList: true,
-      includeListHistory: true,
+      "userId[eq]": useRoute().params.id,
+      history: true,
+    },
+  }
+);
+
+const { data: currentData, pending: currentPending } = myLazyFetch(
+  () => `/api/productlist`,
+  {
+    key: "current",
+    params: {
+      "userId[eq]": useRoute().params.id,
+      current: true,
     },
   }
 );
@@ -123,5 +117,47 @@ const user = computed(() => {
   if (!userData) return {};
   if (!userData.value) return {};
   return userData.value.data;
+});
+
+const current = computed(() => {
+  if (!currentData) return [];
+  if (!currentData.value) return [];
+  return currentData.value.data;
+});
+
+const editionHistory = computed(() => {
+  if (!historyData) return [];
+  if (!historyData.value) return [];
+  const history = historyData.value.data;
+
+  // Get all unique editions
+  let editionIds = [];
+  let editions = [];
+
+  // Get all unique editions
+  history.forEach((item) => {
+    if (!editionIds.includes(item.editionId)) {
+      editionIds.push(item.editionId);
+      editions.push({
+        id: item.editionId,
+        name: item.edition.name,
+        year: item.edition.year,
+        productlists: [],
+      });
+    }
+  });
+
+  // Get all productlists for each edition
+  editions.forEach((edition) => {
+    history.forEach((item) => {
+      if (item.editionId === edition.id) {
+        edition.productlists.push({
+          listNumber: item.listNumber,
+        });
+      }
+    });
+  });
+
+  return editions;
 });
 </script>
