780558170e9ba9a0eefe54445e9c444a147e889f
JorenSynaeveTM <joren.synaeve@thomasmore.be>
2023-03-06 21:32:14 +0100

    Start work admin dashbaord

---
+++ frontend/components/StatCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="stat-card">
+    <div class="stat-card__content">
+      <p class="text-uppercase mb-1 text-muted">{{ title }}</p>
+      <h2><i :class="textIcon"></i> {{ text }}</h2>
+      <div>
+        <span class="text-success text-bold me-1"
+          ><i class="fa fa-arrow-up"></i> +5%</span
+        >
+        <span class="text-muted">vs last 28 days</span>
+      </div>
+    </div>
+    <div class="stat-card__icon" :class="`stat-card__icon--${color}`">
+      <div class="stat-card__icon-circle">
+        <i :class="icon"></i>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: false,
+    default: "",
+  },
+  icon: {
+    type: String,
+    required: false,
+    default: "",
+  },
+  text: {
+    type: String,
+    required: false,
+    default: "",
+  },
+  textIcon: {
+    type: String,
+    required: false,
+    default: "",
+  },
+  color: {
+    type: String,
+    required: false,
+    default: "primary",
+  },
+});
+</script>
--- frontend/pages/admin-dashboard.vue
+++ frontend/pages/admin-dashboard.vue
@@ -3,15 +3,53 @@
     <LayoutPageHeading>
       <template v-slot:title>Beursoverzicht</template>
     </LayoutPageHeading>
-    <p>This is the admin dashboard page</p>
+    <div class="row">
+      <div class="col-3">
+        <StatCard
+          color="success"
+          title="Winst"
+          icon="fa-solid fa-sack-dollar"
+          text-icon="fa-solid fa-euro-sign"
+          text="1.254,00"
+        />
+      </div>
+      <div class="col-3">
+        <StatCard
+          title="Lijsten"
+          text="12"
+          icon="fa-regular fa-rectangle-list"
+          color="danger"
+        />
+      </div>
+      <div class="col-3">
+        <StatCard
+          title="Artikelen"
+          text="12000"
+          icon="fa-solid fa-shirt"
+          color="warning"
+        />
+      </div>
+      <div class="col-3">
+        <StatCard
+          title="Verkocht"
+          text="30"
+          text-icon="fa-solid fa-percent"
+          icon="fa-solid fa-gavel"
+          color="info"
+        />
+      </div>
+    </div>
   </div>
 </template>
 <script setup>
 definePageMeta({
-  layout: 'dashboard',
-  middleware: ['auth'],
+  layout: "dashboard",
+  middleware: ["auth"],
   meta: {
-    authLevel: 'admin',
+    authLevel: "admin",
   },
+});
+useHead({
+  title: "Beursoverzicht",
 });
 </script>
